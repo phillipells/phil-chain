d55e7733b9ce1325ab9a33aecb6b216b66912721
phillipells <phillip.ells1025@gmail.com>
2018-01-31 16:54:33 -0600

    adding package and block class

---
+++ src/block.ts
@@ -0,0 +1,23 @@
+export interface IBlock {
+    index: number;
+    hash: string;
+    previousHash: string;
+    timestamp: number;
+    data: string;
+}
+
+export class Block implements IBlock { 
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+
+    constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+}
